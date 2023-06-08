1594366056e32d90cb4c24dfc1c9427e39f4ca4e
Merk9429 <connor9429@gmail.com>
2023-06-08 14:28:40 +0700

    new

--- NewPortal/NewsPortalBase/News/views.py
+++ NewPortal/NewsPortalBase/News/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import *
 
-# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    ordering = '-datetime'
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'newsone.html'
+    context_object_name = 'newsone'
