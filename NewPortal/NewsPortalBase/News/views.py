505cdff2e12449e4bbc281ee04a1e8785a9d82ce
Merk9429 <connor9429@gmail.com>
2023-06-10 14:43:09 +0700

    new

---
+++ NewPortal/NewsPortalBase/News/views.py
@@ -0,0 +1,105 @@
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+from django.urls import reverse_lazy
+from .models import *
+from .filters import PostFilter
+from .forms import PostForm
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-datetime'
+    template_name = 'news.html'
+    context_object_name = 'news'
+    paginate_by = 10
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'newsone.html'
+    context_object_name = 'newsone'
+
+
+class PostCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_create.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.category = 'NW'
+        return super().form_valid(form)
+
+
+class PostEdit(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.category = 'NW'
+        return super().form_valid(form)
+
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.category = 'NW'
+        return super().form_valid(form)
+
+
+class ArticleCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'article_create.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.category = 'AR'
+        return super().form_valid(form)
+
+
+class ArticleEdit(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'article_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.category = 'AR'
+        return super().form_valid(form)
+
+
+class ArticleDelete(DeleteView):
+    model = Post
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('post_list')
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.category = 'AR'
+        return super().form_valid(form)
+
+
+class PostSearch(ListView):
+    model = Post
+    ordering = '-datetime'
+    template_name = 'post_search.html'
+    context_object_name = 'news'
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
