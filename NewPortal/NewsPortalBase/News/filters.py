db1d775226779a3e945fbac8ca084e8929b6e551
Merk9429 <connor9429@gmail.com>
2023-07-09 22:08:49 +0700

    new

---
+++ NewPortal/NewsPortalBase/News/filters.py
@@ -0,0 +1,16 @@
+import django_filters
+from django import forms
+from News.models import *
+
+
+class PostFilter(django_filters.FilterSet):
+    date = django_filters.DateFilter(field_name="datetime", widget=forms.DateInput(attrs={'type': "date"}),
+                                     label='Дата', lookup_expr='date')
+
+    class Meta:
+        model = Post
+        fields = {
+            'author': ['exact'],
+            'heading': ['icontains'],
+            'category': ['exact'],
+        }
